b2f7002e7bcf5bc93543269b9e29f2640524c2f7
Mahendra <professionalmakbrand@gmail.com>
2026-09-10 22:45:39 +0530

    Bump versionCode to 2 for Google Play Console release

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,7 +11,7 @@ android {
         applicationId = "com.maahi.iractvremote"
         minSdk = 24
         targetSdk = 36
-        versionCode = 1
+        versionCode = 2
         versionName = "1.0"
     }
 
